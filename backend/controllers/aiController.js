80cfac8e5b7c47915a6d2defc94d6c4b31893f97
kousikr163 <kousikblore@gmail.com>
2026-04-06 21:38:10 +0530

    Fix case sensitivity issue in ChatHistory model import

--- backend/controllers/aiController.js
+++ backend/controllers/aiController.js
@@ -1,7 +1,7 @@
 import Document from '../models/Document.js';
 import Flashcard from '../models/Flashcard.js';
 import Quiz from '../models/Quiz.js';
-import ChatHistory from '../models/ChatHistory.js';
+import ChatHistory from '../models/chatHistory.js';
 import * as geminiService from '../utils/geminiService.js';
 import { findRelevantChunks } from '../utils/textChunker.js';
 
